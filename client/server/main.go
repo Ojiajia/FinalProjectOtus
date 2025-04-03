58c97f5eadd999864fcc59d7392706f2b1a15f3f
Ojiajia <bluegraycup@gmail.com>
2025-04-03 12:33:45 +0300

    Update server

--- client/server/main.go
+++ client/server/main.go
@@ -9,35 +9,14 @@ import (
 )
 
 func main() {
-	// staticDir := ""
-	// встроенный обработчик http.FileServer,
-	// для доступа к статическим файлам из
-	// определенной директории по HTTP протоколу:
-
-	//staticDir := "./"
-
-	// чтобы все HTTP-запросы, начинающиеся с "/static/",
-	// обрабатывались с  помощью http.FileServer:
-	// Для инициализации обработчика http.FileServer нужно
-	// использовать функцию http.FileServer() следующим образом:
-
-	//fileServer := http.FileServer(http.Dir(staticDir))
-
 	ip := flag.String("ip", "127.0.0.1", "IP address") //
 	port := flag.String("port", "8080", "Port number")
 	flag.Parse()
-
 	println("run")
-
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	http.HandleFunc("/get_data", setContent) // where setcontent is handler
-
-	// fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	http.ListenAndServe(*ip+":"+*port, nil)
-
+	http.ListenAndServe(*ip+":"+*port, nil
 }
 
 /*
@@ -50,7 +29,6 @@ func setContent(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("error error error")
 	}
-	// w.Header().Set("Content-Type", "image/jpeg")
 	w.Header().Set("Content-Type", "text/txt")
 	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
 	w.Write(data)
@@ -65,7 +43,7 @@ func setContent(w http.ResponseWriter, r *http.Request) {
 	data, err := os.ReadFile("static/mig-31.jpg")
 	if err != nil {
 		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest) // если файла нет, отправляем клиенту 4** ошибку
 		return
 	}
 	w.Header().Set("Content-Type", "image/jpg")
