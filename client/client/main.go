139a88a8f606db742ad26f88de52c8dad7eba23f
Ojiajia <bluegraycup@gmail.com>
2025-04-03 16:45:55 +0300

    this is my commit

--- client/client/main.go
+++ client/client/main.go
@@ -1,22 +1,46 @@
+// + чтоб клиентов было много
+
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+
+	// fmt.Println(text)
+	//запрос на покажити все файлы
 	resp, err := http.Get(
-		"http://localhost:8080/get_data",
+		//	"http://localhost:8080/get_data",
+		text,
 	)
 	if err != nil {
 		fmt.Println("Ошибка запроса", err)
 		return
 	}
 
+	// получить все файлы методом post
+
+	// ввести нужный путь
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+
+	text = strings.TrimSuffix(text, "\n")
+
+	text = "http://localhost:8080/" + text
+
+	// считать данные (.css .html .js .jpg)
+
 	body, err := io.ReadAll(resp.Body)
+
 	if err != nil {
 		fmt.Println("Ошибка чтения", err)
 		return
--- client/server/main.go
+++ client/server/main.go
@@ -3,32 +3,54 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	// "log"
 	"net/http"
 	"os"
 	"strconv"
 )
 
 func main() {
+	// staticDir := ""
+	// встроенный обработчик http.FileServer,
+	// для доступа к статическим файлам из
+	// определенной директории по HTTP протоколу:
+
+	//staticDir := "./"
+
+	// чтобы все HTTP-запросы, начинающиеся с "/static/",
+	// обрабатывались с  помощью http.FileServer:
+	// Для инициализации обработчика http.FileServer нужно
+	// использовать функцию http.FileServer() следующим образом:
+
+	//fileServer := http.FileServer(http.Dir(staticDir))
+
 	ip := flag.String("ip", "127.0.0.1", "IP address") //
 	port := flag.String("port", "8080", "Port number")
 	flag.Parse()
+
 	println("run")
+
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	http.HandleFunc("/get_data", setContent) // where setcontent is handler
-	http.ListenAndServe(*ip+":"+*port, nil
+
+	// fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	http.ListenAndServe(*ip+":"+*port, nil)
+
 }
 
+// принять
 /*
-
-// transfer text
-
-func setContent(w http.ResponseWriter, r *http.Request) {
+func getPath(w http.ResponseWriter, r *http.Request) {
 
 	data, err := os.ReadFile("hello.txt")
 	if err != nil {
 		fmt.Println("error error error")
 	}
+	// w.Header().Set("Content-Type", "image/jpeg")
 	w.Header().Set("Content-Type", "text/txt")
 	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
 	w.Write(data)
@@ -40,12 +62,23 @@ func setContent(w http.ResponseWriter, r *http.Request) {
 
 func setContent(w http.ResponseWriter, r *http.Request) {
 
-	data, err := os.ReadFile("static/mig-31.jpg")
+	data, err := os.ReadFile("static/mig-31.jpg") // тут в скобках строка
 	if err != nil {
 		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusBadRequest) // если файла нет, отправляем клиенту 4** ошибку
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	/*
+
+		entries, err := os.ReadDir("/static")
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	// for _, e := range entries {
+	// 	fmt.Println(e.Name())
+	// }
+
 	w.Header().Set("Content-Type", "image/jpg")
 	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
 	w.Header().Set("Cache-Control", "public, max-age=3600")
